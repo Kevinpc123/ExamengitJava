3f0fd1e59eedf6b7bb0ce007c994cf3a72374f52
Kevinpc123 <120367570+Kevinpc123@users.noreply.github.com>
2023-06-05 12:43:55 +0200

    codigo cambiado

--- primero.java
+++ primero.java
@@ -3,6 +3,7 @@ public static void multiplos(int numeroMax){
         if(i%7==0){
             System.out.println("multiplo de 7:"i+);
         }
+
         if(i%49==0){
             System.out.println("multiplo de 49:"+i);
         }
